944210435fdfa44a7a75e4243a26411358493bd1
Austin Mleziva <AustinMleziva@gmail.com>
2020-04-07 14:44:46 -0500

    Adding workout form initial layout

--- FitnessCelebrity/FitnessCelebrity.Web/ClientApp/src/components/workout/Create-Page.js
+++ FitnessCelebrity/FitnessCelebrity.Web/ClientApp/src/components/workout/Create-Page.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import authService from '../api-authorization/AuthorizeService'
 
 export class CreatePage extends Component {
     static displayName = CreatePage.name;
 
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = { displayExistingPaths: false, displayNewPathForm: false, existingPaths: [] };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.displayExistingPaths = this.displayExistingPaths.bind(this);
+        this.displayNewPathForm = this.displayNewPathForm.bind(this);
+    }
 
     handleChange(event) {
         this.setState({ value: event.target.value });
@@ -20,21 +23,84 @@ export class CreatePage extends Component {
     }
 
     render() {
+        const displayExistingPaths = this.state.displayExistingPaths;
+        const displayNewPathForm = this.state.displayNewPathForm;
+        const Dropdown = ({ options }) =>
+            <select>
+                {options.map((e, i) => <option key={i}>{e}</option>)}
+            </select>
+            ;
+        let pathsDisplay;
+        if (displayExistingPaths) {
+            pathsDisplay = <p><Dropdown options={this.state.existingPaths} /></p>
+        }
+        if (displayNewPathForm) {
+            pathsDisplay = <p>New Paths!</p>
+        }
+      
+
         return (
             <div>
                 <h1>Create a new workout</h1>
 
-                <p>Fitness Path: Add this workout to a new or existing fitness path</p>
-                <button>Button for fitness path</button>
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
+                <p>Fitness Path</p>
+                <button className='btn' onClick={this.displayExistingPaths}>Add this workout to an existing fitness path</button>
+                <button className='btn' onClick={this.displayNewPathForm}>Create new fitness path</button>
+                {pathsDisplay}
+                <form onSubmit={this.handleSubmit}>
+                    <div class="form-group">
+                        <label for="inputName">Workout name</label>
+                        <input id="inputName" className="form-control" type="text" value={this.state.value} onChange={this.handleChange} placeholder="Enter workout name" />
+                        <small id="nameHelp" className="form-text text-muted">Give your workout a name.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputDescription">Workout description</label>
+                        <textarea id="inputDescription" className="form-control" rows="4" placeholder="Enter workout name"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputBody">Workout content</label>
+                        <textarea id="inputBody" className="form-control" rows="4"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="category">Category:</label>
+                        <select className="form-control" id="category">
+                            <option value="instagram">instagram</option>
+                            <option value="youtube">youtube</option>
+                            <option value="google sheets">google</option>
+                            <option value="image">image</option>
+                            <option value="text/other">Other</option>
+                        </select>
+                    </div>
+
+                    <input type="submit" className="btn btn-primary" value="Submit" />
                 </form>
             </div>
 
         );
+    }
+    displayExistingPaths() {
+        let showPaths = this.state.displayExistingPaths
+        if (!showPaths) {
+            //load data from API
+            this.setState({ existingPaths: ["Grass", "Poison", "Fire", "Flying", "Dragon"]});
+        }
+        this.setState({
+            displayExistingPaths: !showPaths,
+            displayNewPathForm: false
+        });
+    }
+    displayNewPathForm() {
+        this.setState({
+            displayExistingPaths: false,
+            displayNewPathForm: !this.state.displayNewPathForm
+        });
+    }
+    async getExistingPaths() {
+        const token = await authService.getAccessToken();
+        const response = await fetch('weatherforecast', {
+            headers: !token ? {} : { 'Authorization': `Bearer ${token}` }
+        });
+        const data = await response.json();
+        this.setState({ forecasts: data, loading: false });
     }
 }
