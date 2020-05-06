876e0341e664ae9dbea61854c4e864eaf9d0c9c1
Austin Mleziva <AustinMleziva@gmail.com>
2020-05-06 11:06:48 -0500

    Adding accordion and tabs to manage page

--- FitnessCelebrity/FitnessCelebrity.Web/ClientApp/src/components/manage/managePage.js
+++ FitnessCelebrity/FitnessCelebrity.Web/ClientApp/src/components/manage/managePage.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import EsTable from "../shared/tables/EsTable";
 import FitnessPathService from "../../services/FitnessPathService";
 import MovementService from "../../services/MovementService";
@@ -12,69 +17,110 @@ export class ManagePage extends Component {
     return (
       <div>
         <h1>Manage Fitness</h1>
-        <div className="row border-bottom">
-          <div className="col-sm-6">
-            <h2>Fitness Paths</h2>
-          </div>
-          <div className="col-sm-6">
-            <Link
-              to="/fitnessPath/create"
-              className="btn btn-outline-primary float-right"
-            >
-              Create New Fitness Path
-            </Link>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <EsTable
-              loadItems={() => FitnessPathService.get()}
-              detailsPath={"fitnessPath"}
-            />
-          </div>
-        </div>
-        <div className="row border-bottom">
-          <div className="col-sm-6">
-            <h2>Workouts</h2>
-          </div>
-          <div className="col-sm-6">
-            <Link
-              to="/workout/create"
-              className="btn btn-outline-primary float-right"
-            >
-              Create New Workout
-            </Link>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <EsTable
-              loadItems={() => WorkoutService.getMyCreatedWorkouts()}
-              detailsPath={"workout"}
-            />
-          </div>
-        </div>
-        <div className="row border-bottom">
-          <div className="col-sm-6">
-            <h2>Movements</h2>
-          </div>
-          <div className="col-sm-6">
-            <Link
-              to="/movement/create"
-              className="btn btn-outline-primary float-right"
-            >
-              Create New Movement
-            </Link>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <EsTable
-              loadItems={() => MovementService.getMyCreatedMovements()}
-              detailsPath={"movement"}
-            />
-          </div>
-        </div>
+
+        <Tabs defaultActiveKey="created" id="uncontrolled-tab-example">
+          <Tab eventKey="created" title="Created">
+            <Accordion defaultActiveKey="0">
+              <Card>
+                <Card.Header>
+                  <div className="row">
+                    <div className="col-sm-1">
+                      <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                        Caret
+                      </Accordion.Toggle>
+                    </div>
+                    <div className="col-sm-5">
+                      <h2>Fitness Paths</h2>
+                    </div>
+                    <div className="col-sm-6">
+                      <Link
+                        to="/fitnessPath/create"
+                        className="btn btn-outline-primary float-right"
+                      >
+                        Create New Fitness Path
+                      </Link>
+                    </div>
+                  </div>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    <EsTable
+                      loadItems={() => FitnessPathService.get()}
+                      detailsPath={"fitnessPath"}
+                    />
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Card.Header>
+                  <div className="row">
+                    <div className="col-sm-1">
+                      <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                        Caret
+                      </Accordion.Toggle>
+                    </div>
+                    <div className="col-sm-5">
+                      <h2>Workouts</h2>
+                    </div>
+                    <div className="col-sm-6">
+                      <Link
+                        to="/workout/create"
+                        className="btn btn-outline-primary float-right"
+                      >
+                        Create New Workout
+                      </Link>
+                    </div>
+                  </div>
+                </Card.Header>
+                <Accordion.Collapse eventKey="1">
+                  <Card.Body>
+                    {" "}
+                    <EsTable
+                      loadItems={() => WorkoutService.getMyCreatedWorkouts()}
+                      detailsPath={"workout"}
+                    />
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Card.Header>
+                  <div className="row">
+                    <div className="col-sm-1">
+                      <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                        Caret
+                      </Accordion.Toggle>
+                    </div>
+                    <div className="col-sm-5">
+                      <h2>Movements</h2>
+                    </div>
+                    <div className="col-sm-6">
+                      <Link
+                        to="/movement/create"
+                        className="btn btn-outline-primary float-right"
+                      >
+                        Create New Movement
+                      </Link>
+                    </div>
+                  </div>
+                </Card.Header>
+                <Accordion.Collapse eventKey="2">
+                  <Card.Body>
+                    <EsTable
+                      loadItems={() => MovementService.getMyCreatedMovements()}
+                      detailsPath={"movement"}
+                    />
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Tab>
+          <Tab eventKey="activity" title="Activity">
+            "thing 2"
+          </Tab>
+          <Tab eventKey="history" title="History">
+            "thing 3"
+          </Tab>
+        </Tabs>
       </div>
     );
   }
